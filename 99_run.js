30b61fb8a5ba1107fe53782b40702a8408290b3b
Yan, Harry <harryan@iu.edu>
2023-06-28 20:47:16 -0400

    frontend protype

---
+++ 99_run.js
@@ -0,0 +1,25 @@
+
+var preload = {
+    type: jsPsychPreload,
+    auto_preload: true
+  };
+
+timeline.push(preload);
+
+jsPsych.run([
+    welcome, 
+    fullscreen_trial, 
+    preload, 
+    browsercheck_instruction, 
+    browser_check_trial, 
+    results_trial, 
+    session_instruction, 
+    cogpsy_q1, 
+    cogpsy_q23,
+    cogpsy_q45,
+    cogpsy_q6,
+    cogpsy_summary,
+    post_trial_instruction,
+    fullscreen_trial_exit
+ ]);
+
